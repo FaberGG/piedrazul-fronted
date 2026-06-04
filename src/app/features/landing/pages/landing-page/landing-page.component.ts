3e80be8e7281187aeac1afef7e8f30c299090819
FaberGG <jhonfabernavia@gmail.com>
2026-06-03 23:17:18 -0500

    feat: update landing and login pages with new logo images and styles

--- src/app/features/landing/pages/landing-page/landing-page.component.ts
+++ src/app/features/landing/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
